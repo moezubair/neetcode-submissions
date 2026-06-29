a20bb2e5e67c6e8e7b27ce2fed8f5ed899ed6664
Zubair <mzubair01@gmail.com>
2026-06-29 17:58:45 -0400

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Input is not sorted
+        ## Can we sort this list and make it a two pointer problem.
+        ## Sorted list: [-4,-1,0,1,2]
+        
+
+        target = 0
+        results = set()
+        nums.sort()
+
+        for i in range(len(nums) -2):
+            j = i+1
+            k = len(nums) - 1
+            # From here, we need to find two numbers in array nums[i+1:] where they sum to x+y=-nums[i]
+            while j < k:
+                sum = nums[i]+nums[j]+nums[k]
+                if sum == target:
+                    triplet = tuple(sorted([nums[i], nums[j], nums[k]]))
+                    results.add(triplet)
+                    j += 1
+                    k -= 1
+                elif sum < 0: #If we need a bigger number move right
+                    j +=1
+                else:
+                    k -=1
+        # Convert set of tuples back to a list of lists
+        return [list(t) for t in results]
